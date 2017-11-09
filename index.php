f7f1322eb840b58b7888b5c647451f3f54e0ac7a
suketa33 <desousajordan@outlook.fr>
2017-11-09 09:49:28 +0100

    rajout de date

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>On utilise Git</h1>
+        <div class="container">
+            <div class="row">
+            <?php echo 'nous sommes le'. date('d-m-Y'); ?>
+                <img src="foto.png">
+                lololololololololololo
+        </div>
+    </div>
+</body>
+</html>
